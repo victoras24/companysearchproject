dfb00496a2da3aa37fa40f5670b97d61edaae026
zebra <victoras.isaakidis@sysdocgroup.com>
2025-08-06 11:33:18 +0300

    changes to the xml file, include the blog page

--- src/pages/Blog/Blog_view.tsx
+++ src/pages/Blog/Blog_view.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { NavLink, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { Calendar, Clock, ArrowRight, FileText } from "lucide-react";
 import "./_blog.css";
 
